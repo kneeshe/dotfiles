7cfed07b6ccc25acd70ab5690bea175148a7c509
Cris <cr.sousa616@gmail.com>
2026-01-01 20:20:56 -0300

    Refatoração do nvim
    Removi o nvim lsp e mason e refatorei o nvim para ser minimalista
    
    Sem os plugins de lsp e mason, removi alguns plugins extras, mantendo apenas os indispensaveis. Adicionei o blink.cmp para ser o novo plugind de autocomplete.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,4 +2,5 @@ require('config.lazy') -- plugin manager
 require('config.options') -- options for the editor
 require('config.keymaps') -- keymaps shortcuts
 require('config.lsp') -- lsp config
--- require('config.diagnostics')
+require("luasnip.loaders.from_vscode").lazy_load()
+
--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -1,4 +1,4 @@
-return {
+return{
 	"L3MON4D3/LuaSnip",
 	-- follow latest release.
 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
